b78262204fda7152785fdd2b14611ed9d038f7bf
aasherkataria <aasherkataria@outlook.com>
2020-12-12 16:56:02 -0600

    addition of xhr request in the api

--- TimeTrackerApi.js
+++ TimeTrackerApi.js
@@ -22,6 +22,21 @@ class TimeTrackerApi {
 				'path' : path,
 				'handler': success_handler});
 		// INSERT YOUR CODE HERE
+		//create an xhr request with the object provided
+		 const xhr = new XMLHttpRequest();
+		 //url object that has base url and path attached
+		 let url = new URL(this.base_url, path);
+		 //create a request 
+		 xhr.open(method, url);
+		 //set response type to json
+		 xhr.responseType = 'json';
+		 //send the request
+		 xhr.send();
+
+		 //on a successful request load the 
+		 xhr.onload = () => {
+			 this.xhrRequestHander(xhr.response);
+		 }
 
 	}
 
