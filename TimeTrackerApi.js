289cff2aedf66350e5f83023a0c97d4819e74011
aasherkataria <aasherkataria@outlook.com>
2020-12-12 19:44:24 -0600

    connects to api now, missing api key

--- TimeTrackerApi.js
+++ TimeTrackerApi.js
@@ -12,20 +12,24 @@ class TimeTrackerApi {
 		this.base_url = base_url;
 
 		// INSERT YOUR CODE HERE
+
 	}
 
 	makeRequest(method, path, parameters = {}, success_handler = false)
 	{
+
 		console.log('----- makeRequest -----',
 			{
 				'method' : method,
 				'path' : path,
-				'handler': success_handler});
+				'handler': success_handler
+			});
 		// INSERT YOUR CODE HERE
 		//create an xhr request with the object provided
-		 const xhr = new XMLHttpRequest();
+		 const xhr = new XMLHttpRequest();;
 		 //url object that has base url and path attached
-		 let url = new URL(this.base_url, path);
+		 let url = new URL(path, this.base_url);
+		 console.log(url);
 		 //create a request 
 		 xhr.open(method, url);
 		 //set response type to json
@@ -36,6 +40,7 @@ class TimeTrackerApi {
 		//  //let the xhrRequestHandler handle onload request
 		//  this.xhrRequestHandler(xhr);
 
+		console.log(xhr.getAllResponseHeaders());
 		xhr.onload = () => {
 			console.log(xhr.response);
 		}
@@ -46,19 +51,10 @@ class TimeTrackerApi {
 	{
 		console.log('----- xhrRequestHandler -----', xhr.responseURL);
 		// INSERT YOUR CODE HERE
-		xhr.onload = () =>
-		{
-			try
-			{
-				success_handler = true;
-			}
-			catch(xhr.response)
-		{
+		xhr.onload = () => {
 			success_handler = false
 		}
-		}
-		xhr.onerror = () =>
-		{
+		xhr.onerror = () => {
 			showError();
 		}
 	}
