7c9c78975475b97d65a100493ee30290929fba9c
navkharoud <navkaransingh831@gmail.com>
2020-12-14 15:59:50 +0100

    wrote the utility methods

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ let my_api_key = 'pr2kn37-qz6fgn0p27rjd5b8-7j1nfw3'; //for workflow to work
  * @var {int} company_id
  * Your company ID, you must replace this is your value once you know your company ID
  */
-let company_id = 0;
+let company_id = 21;
 
 
 /**
@@ -36,33 +36,46 @@ let company_id = 0;
  * profile of who is currently working.
  */
 let my_api = new TimeTrackerApi(my_api_key, api_url);
-my_api.makeRequest('GET','acs/profile', {}, saveUserID);
+my_api.makeRequest('GET', 'acs/profile', {}, saveUserID);
 my_api = null;
 
 
 
-function saveUserID(profile_object)
-{
+function saveUserID(profile_object) {
 	console.log('----- saveUserID -----', profile_object);
 	// INSERT YOUR CODE HERE
 }
 
-function convertSecondsToHoursMinutesSeconds(seconds)
-{
+function convertSecondsToHoursMinutesSeconds(seconds) {
 	console.log('----- convertSecondsToHoursMinutesSeconds -----', seconds);
-	// INSERT YOUR CODE HERE
-
+	// Makes sure parameter is a number
+	seconds = Number(seconds);
+	//Math.Floor returns the largest integer less than or equal to a given number.
+	let hours = Math.floor(d / 3600);
+	let minutes = Math.floor(d % 3600 / 60);
+	let secs = Math.floor(d % 3600 % 60);
+	// use .slice to return 2 characters from the end
+	return ("0" + hours).slice(-2) + ":" + ("0" + minutes).slice(-2) + ":" + ("0" + secs).slice(-2);
 }
 
-function convertTimestampToDateFormat(timestamp)
-{
+function convertTimestampToDateFormat(timestamp) {
 	console.log('----- convertTimestampToDateFormat -----', timestamp);
 	// INSERT YOUR CODE HERE
+	// Create a new JavaScript Date object based on the timestamp
+	// multiplied by 1000 so that the argument is in milliseconds, not seconds.
+    let date = new Date(timestamp * 1000);
+    // proper formatting for month, date, hour, minutes and seconds.
+    let month = date.getMonth()<10 ? "0"+date.getMonth(): date.getMonth();
+    let day = date.getDate()<10 ? "0"+date.getDate(): date.getDate();
+    let hours =date.getHours()<10 ? "0"+date.getHours(): date.getHours();
+    let minutes =date.getMinutes()<10 ? "0"+date.getMinutes(): date.getMinutes();
+    let seconds = date.getSeconds()<10 ? "0"+date.getSeconds(): date.getSeconds();
+
+	return date.getFullYear()+"-"+month+"-"+day+" "+hours+":"+minutes+":"+seconds;
 
 }
 
-function showError(error_details)
-{
+function showError(error_details) {
 
 	console.error('----- showError -----', error_details);
 	// INSERT YOUR CODE HERE
@@ -71,17 +84,17 @@ function showError(error_details)
 	// appending error box in div
 	document.body.appendChild(errorBox);
 	// creating classs for error box to edit in css
-	errorBox.className="error_box";
+	errorBox.className = "error_box";
 	// creating error box heading 
 	let errorBoxHeading = document.createElement('h1');
 	// storing error response code in error code
 	let errorCode = xhr.response.code;
-    // storing error text in error name
+	// storing error text in error name
 	let errorName = xhr.response.error_details;
 	// puuting each error inerrorBoxHeading 
 	errorBoxHeading.innerHTML = `ERROR: ${errorCode} : ${errorName}`;
 	// appending errorBoxHeading in error box 
 	errorBox.appendChild(errorBoxHeading);
-	
+
 }
 
